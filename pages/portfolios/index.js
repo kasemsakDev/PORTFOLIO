0d476974b1c642db5347bc8289ec8a25d679f665
Kasemsak <namekasemsak@gmail.com>
2021-12-23 08:09:29 +0700

    add col

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -2,25 +2,31 @@ import BaseLayout from "../../components/layouts/BaseLayout";
 import BasePage from "../../components/BasePage";
 import Link from "next/link";
 import axios from "axios";
-
+import { Row, Col } from 'reactstrap';
 const Portfolios = ({posts}) => {
 
 
 
   return (
     <BaseLayout>
-      <BasePage>
+          <BasePage>
+             
               <h1>My portfolios.</h1>
               <br></br>
-              <h3 style={{ marginLeft: 30, marginBottom: 10 }}>DEMO - Social Network Application With .Net Core and React</h3>
+                  <h3 style={{ marginLeft: 30, marginBottom: 10 }}>DEMO - Social Network Application With .Net Core and React</h3>
+                  <Row>
               <img src="/images/Social Network Application.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }} />
               <img src="/images/Social Network Application2.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }}/>
               <img src="/images/Social Network Application3.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }}/>
-              <br></br><br></br>
-              Link : <a href="https://myreactivitiesapp.herokuapp.com/" target="_blank" style={{ color: "White" }}>https://myreactivitiesapp.herokuapp.com/</a>
-              <br></br>
-              github : <a href="https://github.com/kasemsakDev/reactivities" target="_blank" style={{ color: "White" }}>https://github.com/kasemsakDev/reactivities</a>
-              <br></br>
+                  </Row>
+                  <Row>
+                      <Col md="12">
+                          Link : <a href="https://myreactivitiesapp.herokuapp.com/" target="_blank" style={{ color: "White" }}>https://myreactivitiesapp.herokuapp.com/</a>
+                      </Col>
+                      <Col md="12">
+                          github : <a href="https://github.com/kasemsakDev/reactivities" target="_blank" style={{ color: "White" }}>https://github.com/kasemsakDev/reactivities</a>
+                      </Col>
+                          </Row>
               <p><b>Description : </b>
                   You can post events or join other people's events. And you can also chat or leave comments about other people's activities. You can track users in the system.
               </p>
@@ -66,18 +72,26 @@ const Portfolios = ({posts}) => {
               <hr></hr>
 
               <br></br>
-              <h3>DEMO - E-commerce app With React Redux, Hooks, ContextAPI, Stripe, Firebase</h3>
+
+                  <h3>DEMO - E-commerce app With React Redux, Hooks, ContextAPI, Stripe, Firebase</h3>
+                  <Row>
               <img src="/images/E-commerce.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }} />
               <img src="/images/E-commerce2.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }} />
-              <img src="/images/E-commerce3.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }} />
-              <br></br><br></br>
-              Link : <a href="https://intense-thicket-02882.herokuapp.com/shop" target="_blank" style={{ color: "White" }}>https://myreactivitiesapp.herokuapp.com/</a>
-              <br></br>
+                      <img src="/images/E-commerce3.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }} />
+                      </Row>
+                  <Row>
+                      <Col md="12">
+                          Link : <a href="https://intense-thicket-02882.herokuapp.com/shop" target="_blank" style={{ color: "White" }}>https://myreactivitiesapp.herokuapp.com/</a>
+                      </Col>
               github : <a href="https://github.com/kasemsakDev/E-commerce-app-With-React" target="_blank" style={{ color: "White" }}>https://github.com/kasemsakDev/E-commerce-app-With-React</a>
-              <br></br>
+                      <Col md="12">
+                      </Col>
+                          </Row>
               <p><b>Description : </b>
                    An e-commerce system that can add products to cart and Google Sign In Authentication. 
                   </p>
+                  <Row>
+                      <Col md="12">
               <h4>Technology Used</h4>
               <ul>
                   <li>
@@ -107,17 +121,23 @@ const Portfolios = ({posts}) => {
                   </li>
               </ul>
               <hr></hr>
-
-              <br></br>
-              <h3>DEMO - Knowledge Management Application</h3>
+                      </Col>
+                    </Row>
+                  <Row>
+                      <h3>DEMO - Knowledge Management Application</h3>
+                  </Row>
+                  <Row>
               <img src="/images/k.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }} />
               <img src="/images/k2.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }} />
               <img src="/images/k3.png" width="400" height="300" style={{ marginLeft: 30, marginBottom: 5 }} />
-              <br></br><br></br>
+                  </Row>
+                  <Row>
+                      <Col md = "12">
               Link : <a href="https://app-km.herokuapp.com/issue.php" target="_blank" style={{ color: "White" }}>https://app-km.herokuapp.com/issue.php</a>
-              <br></br>
+                      </Col>
+                      <Col md ="12">
               github : <a href="https://github.com/kasemsakDev/KM" target="_blank" style={{ color: "White" }}>https://github.com/kasemsakDev/KM</a>
-              <br></br>
+                      </Col></Row>
               <p><b>Description : </b>
                   Develop a system for the Navy It is a system to set goals until the delivery of work. Results are measured in each department in the form of graphs.</p>
               <h4>Technology Used</h4>
@@ -130,7 +150,8 @@ const Portfolios = ({posts}) => {
               </ul>
               <hr></hr>
 
-              <br></br>
+                  <br></br>
+                  
       </BasePage>
     </BaseLayout >
   )
